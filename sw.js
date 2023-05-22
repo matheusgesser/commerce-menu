7fad2da3a7edba8e5a89dd785e733183abf3d8f1
MatheusGesser <matheusmvg@hotmail.com>
2023-05-22 13:04:11 -0300

    feat: add service worker

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'cool-cache';
+
+// Add whichever assets you want to pre-cache here:
+const PRECACHE_ASSETS = [
+    '/android/',
+    '/ios/',
+    '/windows11/',
+    'index.html',
+    'styles.css',
+    'sw.js'
+]
+
+// Listener for the install event - pre-caches our assets list on service worker install.
+self.addEventListener('install', event => {
+    event.waitUntil((async () => {
+        const cache = await caches.open(CACHE_NAME);
+        cache.addAll(PRECACHE_ASSETS);
+    })());
+});
